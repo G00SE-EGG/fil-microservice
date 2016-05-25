a79a6c991f5931b64889530affbc371da9f6d2f7
g00se_egg <simuelhenderson@gmail.com>
2016-05-25 03:32:03 +0000

    updated app.js

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 var express = require("express");
 var app = express();
 
+//set up handdlebars view engine 
+var handlebars = require("express3-handlebars")
+.create({defaultLayout:"main"});//creates view engine 
+app.engine('handlebars', handlebars.engine);//set express to use it by default
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
+
+app.use(express.static, + '/public');
 
 /*This route is the main page, where the files are selected*/
 app.get('/', function(req, res){
-    var html = '<h1>Welcome to the site</h1>';
+    /*var html = '<h1>Welcome to the site</h1>';
     html += '<p>Please use upload tool below to upload a file</p>';
     html += '<input type=file />';
-    html += '<input type=submit value=Upload />';
-   res.send(html); 
+    html += '<input type=submit value=Upload />';*/
+   res.render('home'); 
 });
 
 
 /*Below is middleware for 404 and 500 errors*/
 
 app.use(function(req, res){
-   res.status(404);
-   res.send('<h1>404 - Not Found </h1>');
+    res.status(404);
+   res.render('404');
 });
 
 app.use(function(req, res){
    res.status(500);
-   res.send('<h1>500 - Server Error </h1>');
+   res.render('500');
 });
 
 app.listen(app.get('port'), function(err){
